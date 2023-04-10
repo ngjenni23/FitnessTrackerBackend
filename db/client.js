e089c5d462cd14f9605eef1caa0dd2bf8345930e
jyn <jennifernguyen@Jennifers-MacBook-Pro-2.local>
2023-04-09 22:36:23 -0400

    render external db url

--- db/client.js
+++ db/client.js
@@ -1,7 +1,7 @@
 const { Pool } = require('pg');
 require('dotenv').config();
 
-const connectionString = process.env.DATABASE_URL || 'postgres://localhost:6432/fitnessdev';
+const connectionString = process.env.DATABASE_URL || 'postgres://fitnessdev_9mxk_user:WvwNhDe1iYjpukGBTnsB3FAOHtnMtPbT@dpg-cg9r21hmbg5adg1rd6s0-a.ohio-postgres.render.com/fitnessdev_9mxk?ssl=true'
 
 const client = new Pool({
   connectionString,
